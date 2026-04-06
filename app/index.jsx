7bfa7d772e4cbb2aee87615d085208e75c23e381
MgsTop13 <mgs350084@gmail.com>
2026-04-06 21:24:48 +0000

    Programando um app de celular no celular...

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+Snack
+
+No description
+
+Edit details
+
+Expo Docs
+Save
+Run on device
+Download as zip
+Show embed code
+
+
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { useState } from 'react';
-import McQueen from "../assets/img/McQueen.webp"
+import { useState, useEffect } from 'react';
 
 export default function App() {
   const [count, setCount] = useState(0);
@@ -9,16 +20,30 @@ export default function App() {
   function Click(){
     setCount(count + 1);
   }
+  
+  function farm(){
+      if(count < 20 && count > 10){
+          setCount(count + 1)
+      }
+      
+    
+  }
+  
+  useEffect(() => {
+      setTimeout(() => {
+          farm()
+      }, 1500)
+  }, [count])
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Look My First App!</Text>
-      <Image style={styles.img} source={McQueen}/>
-
+      <Text style={styles.text}>Pages:</Text>
+      
       <View style={styles.div}>
-        <Text style={styles.text} onPress={Click}>Click Me! {count}</Text>
+         <Text style={styles.pagesText}>Click</Text>
+         <Text style={styles.pagesText}>Math</Text>
       </View>
-      <StatusBar style="auto" />
     </View>
 
   );
@@ -34,29 +59,129 @@ const styles = StyleSheet.create({
 
   div:{
     backgroundColor: '#4e4e4e',
-    width: 400,
+    width: 300,
     height: 50,
     borderWidth: 3,
-    borderColor: "e2e2e2"
+    borderColor: "e2e2e2",
+    borderRadius: 15,
+    transition: 350,
+    display: "flex",
+    justifyContent: "space-around",
+    alignItens: "center",
+    flexDirection: "row"
   },
 
   title:{
     color: "#e2e2e2",
     fontSize: 18,
     position: "fixed",
-    top: 10
+    top: 0
   },
 
   text:{
     color: "#e2e2e2",
     fontSize: 18,
     textAlign: "center",
-    paddingTop: 10
+    transition: 350
   },
-
-  img:{
-    marginBottom: 100,
-    width: 300,
-    height: 200
+  
+  pagesText:{
+    color: "#e2e2e2",
+    textAlign: "center",
+    paddingTop: 13,
+    backgroundColor: "red",
+    height: 45
+    
   }
 });
+
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useState, useEffect } from 'react';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function Click(){
+    setCount(count + 1);
+  }
+  
+  function farm(){
+      if(count < 20 && count > 10){
+          setCount(count + 1)
+      }
+      
+    
+  }
+  
+  useEffect(() => {
+      setTimeout(() => {
+          farm()
+      }, 1500)
+  }, [count])
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Look My First App!</Text>
+      <Text style={styles.text}>Pages:</Text>
+      
+      <View style={styles.div}>
+         <Text style={styles.pagesText}>Click</Text>
+         <Text style={styles.pagesText}>Math</Text>
+      </View>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1e1e1e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  div:{
+    backgroundColor: '#4e4e4e',
+    width: 300,
+    height: 50,
+    borderWidth: 3,
+    borderColor: "e2e2e2",
+    borderRadius: 15,
+    transition: 350,
+    display: "flex",
+    justifyContent: "space-around",
+    alignItens: "center",
+    flexDirection: "row"
+  },
+
+  title:{
+    color: "#e2e2e2",
+    fontSize: 18,
+    position: "fixed",
+    top: 0
+  },
+
+  text:{
+    color: "#e2e2e2",
+    fontSize: 18,
+    textAlign: "center",
+    transition: 350
+  },
+  
+  pagesText:{
+    color: "#e2e2e2",
+    textAlign: "center",
+    paddingTop: 13,
+    backgroundColor: "red",
+    height: 45
+    
+  }
+});
+
+No errors, 1 warning
+
+
+
+v54.0.0
+0
